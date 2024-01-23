3c97b948732e896d5ea475ea1b35751353d2759b
GauPanisset <gauthier.panisset@goshaba.com>
2024-01-23 20:05:53 +0100

    fix(number-field-unit): fix the placement of the unit in the number input which was not centered

--- src/business/palette/ui/palette-form-number-field.tsx
+++ src/business/palette/ui/palette-form-number-field.tsx
@@ -29,11 +29,11 @@ const PaletteFormNumberField = <
       name={name}
       render={({ field: { onChange, ...otherFields } }) => (
         <FormItem className="group relative h-full flex-1 grow space-y-0 pt-2 transition-[width,flex] duration-300">
-          <FormLabel className="absolute top-0 max-w-full truncate bg-background px-2 text-xs text-foreground">
+          <FormLabel className="absolute top-0 z-10 max-w-full truncate bg-background px-2 text-xs text-foreground">
             {label}
           </FormLabel>
           <FormControl>
-            <div className="h-full">
+            <div className="relative h-full">
               <Input
                 className="h-full"
                 type="number"
@@ -45,7 +45,7 @@ const PaletteFormNumberField = <
                 }}
                 {...otherFields}
               />
-              <div className="pointer-events-none absolute right-10 top-5 hidden items-center pl-3 group-hover:flex">
+              <div className="pointer-events-none absolute right-10 top-0 hidden h-full items-center pl-3 group-hover:flex">
                 <span className="text-base sm:text-sm">rem</span>
               </div>
             </div>
